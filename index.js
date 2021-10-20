d8cbe417697613b96278fc3cb53e8b7b188c34ac
thienlong306 <phandongthienlong306@gmail.com>
2021-10-20 17:18:25 +0700

    edit last skill

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ var listSkill = [
         progress: "80",
     }
 ]
-var lastSkill = "Also proficient in Adobe Photoshop and Illustrator, grew up bilingual English.";
+var lastSkill = "Also proficient in Adobe Photoshop and Illustrator, English.";
 var listEducation = [
     {
         name: "Trường Đại Học Sài Gòn",
